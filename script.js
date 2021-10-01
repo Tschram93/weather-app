5532cb77ec1652c71f62fba6be21071c7af24ce1
TS93 <tschram93@yahoo.com>
2021-09-30 20:42:23 -0400

    Planned js with psuedo

---
+++ script.js
@@ -0,0 +1,31 @@
+// Create Class selector variables
+
+
+// assign "openweatheapi" url variable
+// assign queries variables
+
+function currentDayWeather(citySearch) {
+    // save api key into variable
+
+    let api = ``;
+};
+
+// Fetch 'OpenWeatherAPI"
+
+// Metric to imperial measurements
+
+// AddEventListener to search button
+// IF search button click search
+
+// Current day (city name) with date
+// TEMP, WIND, HUMIDITY, UV INDEX from api for main card(current day)
+
+// 5 day Forecast info fetched for
+// Date
+// TEMP, WIND, HUMIDITY
+
+// Searches saved to localStorage
+// Local Storage content displayed under Search button
+// if searches add .. else
+
+// Error catch
