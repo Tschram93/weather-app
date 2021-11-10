906a5524c729784ae3fe42f0d2e1bd7cb8e3b32a
TS93 <tschram93@yahoo.com>
2021-11-09 20:23:58 -0500

    corrected forecasting width to fit screen without overlap

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const submitBtn = document.querySelector('.submitBtn');
 const inputValue = document.querySelector('#inputValue');
 
+
 // Current Day Weather Card
 const todaysCard = document.querySelector('#todaysCard');
 const currentDate = document.querySelector('#currentDate');
@@ -23,46 +24,56 @@ submitBtn.addEventListener('click', () => {
             let tempValue1 = data['list'][4]['main']['temp'];
             let windValue1 = data['list'][4]['wind']['speed'];
             let humidityValue1 = data['list'][4]['main']['humidity'];
+            let iconValue1 = data['list'][4]['weather']['icon'];
             let tempValue2 = data['list'][12]['main']['temp'];
             let windValue2 = data['list'][12]['wind']['speed'];
             let humidityValue2 = data['list'][12]['main']['humidity'];
+            let iconValue2 = data['list'][12]['weather']['icon'];
             let tempValue3 = data['list'][20]['main']['temp'];
             let windValue3 = data['list'][20]['wind']['speed'];
             let humidityValue3 = data['list'][20]['main']['humidity'];
+            let iconValue3 = data['list'][20]['weather']['icon'];
             let tempValue4 = data['list'][28]['main']['temp'];
             let windValue4 = data['list'][28]['wind']['speed'];
             let humidityValue4 = data['list'][28]['main']['humidity'];
+            let iconValue4 = data['list'][28]['weather']['icon'];
             let tempValue5 = data['list'][36]['main']['temp'];
             let windValue5 = data['list'][36]['wind']['speed'];
             let humidityValue5 = data['list'][36]['main']['humidity'];
+            let iconValue5 = data['list'][36]['weather']['icon'];
             // Forecast
             // Day 1
             temp1.innerText = tempValue1;
             wind1.innerText = windValue1;
             humidity1.innerText = humidityValue1;
+            
             //Day2
             temp2.innerText = tempValue2;
-            windValue2.innerText = windValue2;
+            wind2.innerText = windValue2;
             humidity2.innerText = humidityValue2;
+            
             // Day 3
             temp3.innerText = tempValue3;
-            windValue3.innerText = windValue3;
+            wind3.innerText = windValue3;
             humidity3.innerText = humidityValue3;
+            
             //Day 4
             temp4.innerText = tempValue4;
-            windValue4.innerText = windValue4;
+            wind4.innerText = windValue4;
             humidity4.innerText = humidityValue4;
+            
             //Day 5
             temp5.innerText = tempValue5;
-            windValue5.innerText = windValue5;
+            wind5.innerText = windValue5;
             humidity5.innerText = humidityValue5;
+            
         })
         .catch(err => console.log('Need to Enter a valid city'))
 });
 
 // Day 1
 const day1Card = document.querySelector('#day1');
-const icon1 = document.querySelector('#icon1').innerContent;
+const icon1 = document.querySelector('#icon1');
 const date1 = document.querySelector('#date1');
 const temp1 = document.querySelector('#temp1');
 const wind1 = document.querySelector('#wind1');
@@ -70,7 +81,7 @@ const humidity1 = document.querySelector('#humidity1');
 
 // Day 2
 const day2Card = document.querySelector('#day2');
-const icon2 = document.querySelector('#icon2').innerContent;
+const icon2 = document.querySelector('#icon2');
 const date2 = document.querySelector('#date2');
 const temp2 = document.querySelector('#temp2');
 const wind2 = document.querySelector('#wind2');
@@ -78,7 +89,7 @@ const humidity2 = document.querySelector('#humidity2');
 
 // Day 3
 const day3Card = document.querySelector('#day3');
-const icon3 = document.querySelector('#icon3').innerContent;
+const icon3 = document.querySelector('#icon3');
 const date3 = document.querySelector('#date3');
 const temp3 = document.querySelector('#temp3');
 const wind3 = document.querySelector('#wind3');
@@ -86,7 +97,7 @@ const humidity3 = document.querySelector('#humidity3');
 
 // Day 4
 const day4Card = document.querySelector('#day4');
-const icon4 = document.querySelector('#icon4').innerContent;
+const icon4 = document.querySelector('#icon4');
 const date4 = document.querySelector('#date4');
 const temp4 = document.querySelector('#temp4');
 const wind4 = document.querySelector('#wind4');
@@ -94,7 +105,7 @@ const humidity4 = document.querySelector('#humidity4');
 
 // Day 5
 const day5Card = document.querySelector('#day5');
-const icon5 = document.querySelector('#icon5').innerContent;
+const icon5 = document.querySelector('#icon5');
 const date5 = document.querySelector('#date5');
 const temp5 = document.querySelector('#temp5');
 const wind5 = document.querySelector('#wind5');
